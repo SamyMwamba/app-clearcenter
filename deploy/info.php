6a58cc591384559aad010e98cde9f29c04d39ee3
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-03-30 15:16:43 +0000

    - Bumped version down to standard 1.0.10 (not yet released)

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'clearcenter';
-$app['version'] = '1.0.11';
+$app['version'] = '1.0.10';
 $app['release'] = '1';
 $app['vendor'] = 'ClearCenter';
 $app['packager'] = 'ClearCenter';
--- packaging/app-clearcenter.spec
+++ packaging/app-clearcenter.spec
@@ -1,7 +1,7 @@
 
 Name: app-clearcenter
 Epoch: 1
-Version: 1.0.11
+Version: 1.0.10
 Release: 1%{dist}
 Summary: ClearCenter Base
 License: Proprietary
