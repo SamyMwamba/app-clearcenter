e02a55f11181638b527ffb598a23c11994fb7fd4
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-06-22 15:39:20 +0000

    - Prep for 6.3.0 Beta 1

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'clearcenter';
-$app['version'] = '1.1.0';
+$app['version'] = '1.2.2';
 $app['release'] = '1';
 $app['vendor'] = 'ClearCenter';
 $app['packager'] = 'ClearCenter';
--- packaging/app-clearcenter.spec
+++ packaging/app-clearcenter.spec
@@ -1,7 +1,7 @@
 
 Name: app-clearcenter
 Epoch: 1
-Version: 1.1.0
+Version: 1.2.2
 Release: 1%{dist}
 Summary: ClearCenter Base
 License: Proprietary
