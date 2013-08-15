4b2d6be047feea1eb7ce3b526c46adbf0fd45cae
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-08-15 01:50:01 +0000

    - Added stub for license engine

---
+++ libraries/License_Engine.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Core engine class for paid app API.
+ *
+ * @category   apps
+ * @package    clearcenter
+ * @subpackage libraries
+ * @author     ClearCenter <developer@clearcenter.com>
+ * @copyright  2013 ClearCenter
+ * @license    http://www.clearcenter.com/app_license ClearCenter license
+ * @link       http://www.clearcenter.com/support/documentation/clearos/clearcenter/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+// N A M E S P A C E
+///////////////////////////////////////////////////////////////////////////////
+
+namespace clearos\apps\clearcenter;
+
+///////////////////////////////////////////////////////////////////////////////
+// B O O T S T R A P
+///////////////////////////////////////////////////////////////////////////////
+
+$bootstrap = getenv('CLEAROS_BOOTSTRAP') ? getenv('CLEAROS_BOOTSTRAP') : '/usr/clearos/framework/shared';
+require_once $bootstrap . '/bootstrap.php';
+
+///////////////////////////////////////////////////////////////////////////////
+// T R A N S L A T I O N S
+///////////////////////////////////////////////////////////////////////////////
+
+clearos_load_language('clearcenter');
+
+///////////////////////////////////////////////////////////////////////////////
+// D E P E N D E N C I E S
+///////////////////////////////////////////////////////////////////////////////
+
+use \clearos\apps\base\Engine as Engine;
+
+clearos_load_library('base/Engine');
+
+///////////////////////////////////////////////////////////////////////////////
+// C L A S S
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Core engine class for paid app API.
+ *
+ * @category   apps
+ * @package    clearcenter
+ * @subpackage libraries
+ * @author     ClearCenter <developer@clearcenter.com>
+ * @copyright  2013 ClearCenter
+ * @license    http://www.clearcenter.com/app_license ClearCenter license
+ * @link       http://www.clearcenter.com/support/documentation/clearos/clearcenter/
+ */
+
+class License_Engine extends Engine
+{
+    ///////////////////////////////////////////////////////////////////////////////
+    // M E T H O D S
+    ///////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * License engine constructor.
+     */
+
+    public function __construct()
+    {
+        clearos_profile(__METHOD__, __LINE__);
+    }
+
+    /**
+     * Returns status of license limit.
+     *
+     * @return boolean TRUE if limit reached or exceeded
+     * @throws Validation_Exception
+     */
+
+    public function limit_reached()
+    {
+        clearos_profile(__METHOD__, __LINE__);
+
+        return TRUE;
+    }
+}
