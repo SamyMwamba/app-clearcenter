8a52f87eaa2bb3419952d9d26785d3c726d5fbe2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:20:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Sistem asas menyediakan teras set alat untuk perisian dari ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Menyambung ke ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Sambungan gagal:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS gagal.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Maklumat dari rangkaian jauh tidak boleh didapati.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Baki Lesen Pengguna:';
+$lang['clearcenter_service_is_up_to_date'] = 'Perkhidmatan up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Perkhidmatan memerlukan kemas kini.';
+$lang['clearcenter_service_update_failed:'] = 'maklumat Perkhidmatan gagal:';
+$lang['clearcenter_subscription_update_complete'] = 'maklumat langganan lengkap';
+$lang['clearcenter_system_not_registered'] = 'Sistem tidak berdaftar.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Tidak dapat menghubungi pelayan jauh.';
