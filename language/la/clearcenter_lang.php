e15747eb21a19da8f73662f092fcc937f09708ae
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:18:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Basi statuto de instrumenta software ratio praebet Core de ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter colligationem ...';
+$lang['clearcenter_connection_failed:'] = 'Defecit nexu:';
+$lang['clearcenter_dns_lookup_failed'] = 'Br DNS defecerunt.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Notitia ex longinquioribus ipsum non competit.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Reliqua licenciis User:';
+$lang['clearcenter_service_is_up_to_date'] = 'Famulatus autem sursum-ut-balanus.';
+$lang['clearcenter_service_requires_an_update'] = 'Service renovatio requiritur.';
+$lang['clearcenter_service_update_failed:'] = 'Service non update:';
+$lang['clearcenter_subscription_update_complete'] = 'The New Subscription';
+$lang['clearcenter_system_not_registered'] = 'Ratio est, non est relatus.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Illi remota contactus server.';
