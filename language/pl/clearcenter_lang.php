54313239f4590ead751d2a2047592116ffec1739
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:21:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Podstawowy system zapewnia rdzeń zestaw narzędzi dla oprogramowania z ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter bazowa';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Podłączanie do ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Połączenie nieudane:';
+$lang['clearcenter_dns_lookup_failed'] = 'wyszukiwanie DNS nie powiodło się.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Informacje od sieci zdalnej nie jest dostępna.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Pozostałe licencje użytkowników:';
+$lang['clearcenter_service_is_up_to_date'] = 'Usługa jest up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Usługa wymaga aktualizacji.';
+$lang['clearcenter_service_update_failed:'] = 'Usługa aktualizacji nie powiodło się:';
+$lang['clearcenter_subscription_update_complete'] = 'Subskrypcja pełna aktualizacja';
+$lang['clearcenter_system_not_registered'] = 'System nie jest zarejestrowany.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Nie można skontaktować się ze zdalnym serwerem.';
