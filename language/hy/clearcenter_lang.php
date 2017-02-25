99085c85f67643813532f2a9332d8042e83a1175
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:12:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Բազային համակարգը ապահովում է հիմնական փաթեթը գործիքների ծրագրային ClearCenter:';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Միանում է ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Կապը ձախողվեց:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS Փնտրել ձախողվեց:';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Տեղեկություն հեռավոր ցանցի հասանելի չէ:';
+$lang['clearcenter_remaining_user_licenses:'] = 'Մնացյալ User լիցենզիաների';
+$lang['clearcenter_service_is_up_to_date'] = 'Service up-to- ամսաթիվը:';
+$lang['clearcenter_service_requires_an_update'] = 'Ծառայությունից օգտվելու համար անհրաժեշտ է թարմացում:';
+$lang['clearcenter_service_update_failed:'] = 'Ծառայության թարմացումը ձախողվեց:';
+$lang['clearcenter_subscription_update_complete'] = 'Բաժանորդագրում թարմացում ամբողջական';
+$lang['clearcenter_system_not_registered'] = 'Համակարգը չի գրանցվել:';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Անհնար կապնվել հեռավոր սերվերի.';
