a70fdae3478c78c9a477cdd6a02c0f893fb202f0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:23:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Basen erbjuder en uppsättning verktyg för programvara från ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Anslutning till ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Anslutningen misslyckades:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS-sökning misslyckades.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Information från fjärrnätverk är inte tillgänglig.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Återstående användarlicenser:';
+$lang['clearcenter_service_is_up_to_date'] = 'Servicen är up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Tjänsten kräver en uppdatering.';
+$lang['clearcenter_service_update_failed:'] = 'Uppdateringstjänsten misslyckades:';
+$lang['clearcenter_subscription_update_complete'] = 'Prenumeration uppdatering komplett';
+$lang['clearcenter_system_not_registered'] = 'Systemet är inte registrerad.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Det går inte att kontakta fjärrserver.';
