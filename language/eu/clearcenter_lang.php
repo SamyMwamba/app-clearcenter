34ca7a74519e1f00cc646de8e9497cbc0f795812
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:13:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'oinarrizko sistema tresna multzo ClearCenter batetik software for core bat eskaintzen du.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter konektatzen ...';
+$lang['clearcenter_connection_failed:'] = 'Konexioak huts egin du:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS bilaketak huts.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'urruneko saretik Informazioa ez dago eskuragarri.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Erabiltzaile lizentziak Falta:';
+$lang['clearcenter_service_is_up_to_date'] = 'Zerbitzua up-to-data.';
+$lang['clearcenter_service_requires_an_update'] = 'Zerbitzua eguneratzea eskatzen du.';
+$lang['clearcenter_service_update_failed:'] = 'Zerbitzua eguneratzea huts egin du:';
+$lang['clearcenter_subscription_update_complete'] = 'Harpidetza eguneratzea';
+$lang['clearcenter_system_not_registered'] = 'Sistema ez dago izen hori.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Ezin izan da urruneko zerbitzariarekin kontaktatu.';
