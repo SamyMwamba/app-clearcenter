dcb6c0c17b48e95dcc8baccb0d00a2e4ac75208f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:20:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Is-sistema bażi tipprovdi qofol sett ta &#39;għodod għas-softwer minn ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Bażi';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Inqarrbu l ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Konnessjoni naqset:';
+$lang['clearcenter_dns_lookup_failed'] = 'Lookup DNS fallew.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Informazzjoni minn netwerk remot mhix disponibbli.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Jifdal Liċenzji User:';
+$lang['clearcenter_service_is_up_to_date'] = 'Servizz huwa up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Servizz teħtieġ aġġornament.';
+$lang['clearcenter_service_update_failed:'] = 'aġġornament Servizz naqset:';
+$lang['clearcenter_subscription_update_complete'] = 'aġġornament Abbonament kompluta';
+$lang['clearcenter_system_not_registered'] = 'Sistema mhix reġistrata.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Jistgħu jikkuntattjaw server remot.';
