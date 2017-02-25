7133bf5f8c8e43849f806d3c4eab70dd4d88e0bb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:21:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Het basissysteem biedt een kern set tools voor software van ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Verbinding maken met ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Verbinding mislukt:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS-lookup is mislukt.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Informatie van extern netwerk is niet beschikbaar.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Resterende Gebruikerslicenties:';
+$lang['clearcenter_service_is_up_to_date'] = 'Service is up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Dienst vereist een update.';
+$lang['clearcenter_service_update_failed:'] = 'Service update is mislukt:';
+$lang['clearcenter_subscription_update_complete'] = 'Abonnementsupdate compleet';
+$lang['clearcenter_system_not_registered'] = 'Systeem is niet geregistreerd.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Kan geen contact met externe server.';
