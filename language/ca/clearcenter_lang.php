1c349d3c3abcedfed9ee07cafa9bf3ceee2c8dbb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:14:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'El sistema de base proporciona un conjunt bàsic d&#39;eines de programari des ClearCenter.';
+$lang['clearcenter_app_name'] = 'base ClearCenter';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Connexió a ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Connexió fallida:';
+$lang['clearcenter_dns_lookup_failed'] = 'recerca de DNS ha fallat.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Informació de xarxa remot no està disponible.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Restant Llicències d&#39;usuari:';
+$lang['clearcenter_service_is_up_to_date'] = 'El servei és de fins al dia.';
+$lang['clearcenter_service_requires_an_update'] = 'Servei requereix una actualització.';
+$lang['clearcenter_service_update_failed:'] = 'actualització de servei va fallar:';
+$lang['clearcenter_subscription_update_complete'] = 'actualització de subscripció completa';
+$lang['clearcenter_system_not_registered'] = 'El sistema no s&#39;ha registrat.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'No s&#39;ha pogut contactar amb el servidor remot.';
