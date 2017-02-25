3be12f407bb5847916c0502faaf45a25b1efbf44
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:22:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Základný systém poskytuje základnú sadu nástrojov pre softvér od ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Základňa';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Pripojenie k ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Pripojenie zlyhalo:';
+$lang['clearcenter_dns_lookup_failed'] = 'zlyhalo vyhľadávania DNS.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Informácie z vzdialenej siete nie je k dispozícii.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Zostávajúce používateľských licencií:';
+$lang['clearcenter_service_is_up_to_date'] = 'Služba je up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Servis vyžaduje aktualizáciu.';
+$lang['clearcenter_service_update_failed:'] = 'Aktualizácia Service zlyhala:';
+$lang['clearcenter_subscription_update_complete'] = 'Aktualizácia predplatné kompletné';
+$lang['clearcenter_system_not_registered'] = 'Systém nie je registrovaný.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Nemožno kontaktovať vzdialený server.';
