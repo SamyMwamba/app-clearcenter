f90b442f93a42c0fb85fb3e5a1f242633caef56e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:22:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Sistemul de bază asigură un nucleu set de instrumente pentru software-ul de la ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Conectarea la ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Conexiune esuata:';
+$lang['clearcenter_dns_lookup_failed'] = 'Căutarea DNS a eșuat.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Informații de la rețea la distanță nu este disponibilă.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Rămase licențe de utilizator:';
+$lang['clearcenter_service_is_up_to_date'] = 'Service-ul este de până la zi.';
+$lang['clearcenter_service_requires_an_update'] = 'Serviciul necesită o actualizare.';
+$lang['clearcenter_service_update_failed:'] = 'Serviciu de actualizare a eșuat:';
+$lang['clearcenter_subscription_update_complete'] = 'actualizare de abonament complet';
+$lang['clearcenter_system_not_registered'] = 'Sistemul nu este înregistrat.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Imposibil de contactat serverul de la distanță.';
