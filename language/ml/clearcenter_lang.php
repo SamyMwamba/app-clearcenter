af48718b69bd6cb1cb9fac79cb01b8d7daf331cb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:20:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'അടിസ്ഥാന സിസ്റ്റം ClearCenter നിന്ന് സോഫ്റ്റ്വെയർ ഉപകരണങ്ങൾ ഒരു കോർ സെറ്റ് നൽകുന്നു.';
+$lang['clearcenter_app_name'] = 'ClearCenter ബേസ്';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter ലേക്ക് ബന്ധിപ്പിക്കുന്നു ...';
+$lang['clearcenter_connection_failed:'] = 'കണക്ഷൻ പരാജയപ്പെട്ടു:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS തിരയൽ പരാജയപ്പെട്ടു.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'വിദൂര നെറ്റ്വർക്കിൽ നിന്നുള്ള വിവരങ്ങൾ ലഭ്യമല്ല.';
+$lang['clearcenter_remaining_user_licenses:'] = 'ശേഷിക്കുന്ന ഉപയോക്തൃ ലൈസൻസുകൾ:';
+$lang['clearcenter_service_is_up_to_date'] = 'സേവന അപ്-റ്റു-തീയതി ആണ്.';
+$lang['clearcenter_service_requires_an_update'] = 'സേവനം ഒരു അപ്ഡേറ്റ് ആവശ്യമാണ്.';
+$lang['clearcenter_service_update_failed:'] = 'സേവന അപ്ഡേറ്റ് പരാജയപ്പെട്ടു:';
+$lang['clearcenter_subscription_update_complete'] = 'പൂർണ്ണമായ സബ്സ്ക്രിപ്ഷൻ അപ്ഡേറ്റ്';
+$lang['clearcenter_system_not_registered'] = 'സിസ്റ്റം രജിസ്റ്റർ ചെയ്തിട്ടില്ല.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'വിദൂര സെർവറിൽ ബന്ധപ്പെടാൻ കഴിയുന്നില്ല.';
