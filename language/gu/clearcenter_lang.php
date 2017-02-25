7a5314040119981e55f08cac4f8a119312389d6b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:16:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'આધાર સિસ્ટમ એક કોર ClearCenter માંથી સોફ્ટવેર માટે સાધનો સમૂહ પૂરો પાડે છે.';
+$lang['clearcenter_app_name'] = 'ClearCenter આધાર';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter માટે કનેક્ટિંગ ...';
+$lang['clearcenter_connection_failed:'] = 'જોડાણ નિષ્ફળ:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS લુકઅપ નિષ્ફળ ગયું.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'રિમોટ નેટવર્ક માંથી માહિતી ઉપલબ્ધ નથી.';
+$lang['clearcenter_remaining_user_licenses:'] = 'વપરાશકર્તા લાઇસેંસીસ બાકી:';
+$lang['clearcenter_service_is_up_to_date'] = 'સેવા-ટુ-ડેટ છે.';
+$lang['clearcenter_service_requires_an_update'] = 'સેવા અપડેટની જરૂર છે.';
+$lang['clearcenter_service_update_failed:'] = 'સેવા અપડેટ નિષ્ફળ ગયું:';
+$lang['clearcenter_subscription_update_complete'] = 'ઉમેદવારી અપડેટ સંપૂર્ણ';
+$lang['clearcenter_system_not_registered'] = 'સિસ્ટમ રજીસ્ટર થયેલ નથી.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'દૂરસ્થ સર્વર સંપર્ક કરવામાં અક્ષમ છે.';
