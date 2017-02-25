2a939b431a25ac7be876ab142f0be002bfacdd10
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:18:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = '기본 시스템은 ClearCenter의 소프트웨어 도구 세트 코어를 제공한다.';
+$lang['clearcenter_app_name'] = 'ClearCenter 자료';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter에 연결 ...';
+$lang['clearcenter_connection_failed:'] = '연결에 실패:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS 조회에 실패했습니다.';
+$lang['clearcenter_information_from_remote_network_not_available'] = '원격 네트워크에서 정보를 사용할 수 없습니다.';
+$lang['clearcenter_remaining_user_licenses:'] = '사용자 라이센스 남은 :';
+$lang['clearcenter_service_is_up_to_date'] = '서비스는 최신입니다.';
+$lang['clearcenter_service_requires_an_update'] = '서비스 업데이트가 필요합니다.';
+$lang['clearcenter_service_update_failed:'] = '서비스 업데이트를 실패했습니다 :';
+$lang['clearcenter_subscription_update_complete'] = '구독 업데이트 완료';
+$lang['clearcenter_system_not_registered'] = '시스템이 등록되어 있지 않습니다.';
+$lang['clearcenter_unable_to_contact_remote_server'] = '원격 서버에 연결할 수 없습니다.';
