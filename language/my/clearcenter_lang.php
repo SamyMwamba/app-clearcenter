43895a19d561f8c066c7ec0cdde47cda818344f5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:13:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'ခြေရင်းစနစ်က ClearCenter မှ software များအဘို့ tools တွေကိုတစ်ဦး core ကို set ကိုပေးစွမ်းသည်။';
+$lang['clearcenter_app_name'] = 'ClearCenter အခြေစိုက်စခန်း';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter ချိတ်ဆက်ခြင်း ...';
+$lang['clearcenter_connection_failed:'] = 'Connection ကိုမအောင်မြင်ပါ:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS ကို lookup ပြုလုပ်နေပျက်ကွက်ခဲ့သည်။';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'ဝေးလံခေါင်သီကွန်ယက်မှပြန်ကြားရေးရရှိနိုင်ပါသည်မဟုတ်။';
+$lang['clearcenter_remaining_user_licenses:'] = 'ကျန်ရှိသောအသုံးပြုသူလိုင်စင်များ:';
+$lang['clearcenter_service_is_up_to_date'] = 'ဝန်ဆောင်မှုကို up-to-date ဖြစ်အောင်ဖြစ်ပါတယ်။';
+$lang['clearcenter_service_requires_an_update'] = 'Service ကို update တစ်ခုလိုအပ်သည်။';
+$lang['clearcenter_service_update_failed:'] = 'Service ကို update ကိုမအောင်မြင်ပါ:';
+$lang['clearcenter_subscription_update_complete'] = 'ပြီးပြည့်စုံသောစာရင်းပေးသွင်း update ကို';
+$lang['clearcenter_system_not_registered'] = 'System ကိုမှတ်ပုံတင်ထားခြင်းမရှိပါ။';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'ဝေးလံခေါင်သီ server ကိုဆက်သွယ်ပါရန်မဖြစ်နိုင်ပါ။';
