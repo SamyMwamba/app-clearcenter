9ffeeb17044269de2eb88f9a0f715aeb6b00cadd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:12:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'baza sistemi ClearCenter proqram üçün alətlər müəyyən bir əsas verir.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter qoşulur ...';
+$lang['clearcenter_connection_failed:'] = 'Connection uğursuz oldu:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS Sistemi bilmədi.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'uzaq şəbəkə məlumat mövcud deyil.';
+$lang['clearcenter_remaining_user_licenses:'] = 'İstifadəçi lisenziya Qalan:';
+$lang['clearcenter_service_is_up_to_date'] = 'Service up-to-tarix deyil.';
+$lang['clearcenter_service_requires_an_update'] = 'Service yeniləmə tələb edir.';
+$lang['clearcenter_service_update_failed:'] = 'Service yeniləmə uğursuz oldu:';
+$lang['clearcenter_subscription_update_complete'] = 'Subscription yeniləmə tam';
+$lang['clearcenter_system_not_registered'] = 'System qeydə deyil.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'uzaq server əlaqə bilmir.';
