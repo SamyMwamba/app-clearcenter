9e428f1ae667c3176230cbca60fe8c6d55a7f023
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:26:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Awọn mimọ eto pese a mojuto ṣeto ti irinṣẹ fun software lati ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Mimọ';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Pọ si ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Asopọ kuna:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS jade kuna.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Alaye lati latọna nẹtiwọki ni ko wa.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Ti o ku User iwe-aṣẹ:';
+$lang['clearcenter_service_is_up_to_date'] = 'Service jẹ soke-to-ọjọ.';
+$lang['clearcenter_service_requires_an_update'] = 'Service nbeere ohun imudojuiwọn.';
+$lang['clearcenter_service_update_failed:'] = 'Service imudojuiwọn kuna:';
+$lang['clearcenter_subscription_update_complete'] = 'Alabapin imudojuiwọn pari';
+$lang['clearcenter_system_not_registered'] = 'Eto ti wa ni ko aami.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Lagbara lati kan si latọna server.';
