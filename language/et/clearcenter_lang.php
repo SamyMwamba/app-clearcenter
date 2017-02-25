be349388a5fb3686c6f1eef08b241776f0a5d748
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:15:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Baasi süsteem pakub tuum tööriistade komplekt tarkvara ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Ühendamine ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Ühendus nurjus:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS ebaõnnestus.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Teave võrgus ei ole kättesaadav.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Ülejäänud tajalitsentside:';
+$lang['clearcenter_service_is_up_to_date'] = 'Teenus on up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Teenindus on vaja ajakohastada.';
+$lang['clearcenter_service_update_failed:'] = 'Teenuse värskendamine nurjus:';
+$lang['clearcenter_subscription_update_complete'] = 'Tellimus uuendus täielik';
+$lang['clearcenter_system_not_registered'] = 'Süsteem ei ole registreeritud.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Ei saa ühendust serveri.';
