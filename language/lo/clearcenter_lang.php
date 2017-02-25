cf8c8435fbd830e4d1662141543be3c8e7d12c6c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:19:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'ລະບົບຖານໃຫ້ຫຼັກທີ່ກໍານົດໄວ້ຂອງເຄື່ອງມືສໍາລັບການຊອບແວຈາກ ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter ຖານ';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ການເຊື່ອມຕໍ່ກັບ ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'ການເຊື່ອມຕໍ່ສົບຜົນສໍາເລັດ:';
+$lang['clearcenter_dns_lookup_failed'] = 'ຄົ້ນຫາ DNS ສົບຜົນສໍາເລັດ.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'ຂໍ້ມູນຈາກເຄືອຂ່າຍຂອງຫ່າງໄກສອກຫຼີກບໍ່ສາມາດໃຊ້.';
+$lang['clearcenter_remaining_user_licenses:'] = 'ທີ່ຍັງເຫຼືອໃບອະນຸຍາດຜູ້ໃຊ້:';
+$lang['clearcenter_service_is_up_to_date'] = 'ການບໍລິການແມ່ນຂຶ້ນກັບ, ວັນທີ.';
+$lang['clearcenter_service_requires_an_update'] = 'ການບໍລິການຮຽກຮ້ອງໃຫ້ມີການປັບປຸງ.';
+$lang['clearcenter_service_update_failed:'] = 'ການປັບປຸງການບໍລິການສົບຜົນສໍາເລັດ:';
+$lang['clearcenter_subscription_update_complete'] = 'ການປັບປຸງການສະຫມັກຄົບຖ້ວນສົມບູນ';
+$lang['clearcenter_system_not_registered'] = 'ລະບົບບໍ່ໄດ້ລົງທະບຽນ.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'ບໍ່ສາມາດທີ່ຈະຕິດຕໍ່ຂອງເຄື່ອງແມ່ຂ່າຍຫ່າງໄກສອກຫຼີກ.';
