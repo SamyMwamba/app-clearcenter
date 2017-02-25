68f9d7ae3428ed846f12c535cc12abe17f21f82e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:17:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'The isi usoro na-enye a isi set ngwaọrụ maka software si ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Ejikọta ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Njikọ okpu:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS Nchọgharị na-emezughị.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Ozi si n&#39;ime obodo na netwọk bụ adịghị.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Fọdụrụ User ikikere:';
+$lang['clearcenter_service_is_up_to_date'] = 'Service bụ elu na-ụbọchị.';
+$lang['clearcenter_service_requires_an_update'] = 'Service-achọ ihe update.';
+$lang['clearcenter_service_update_failed:'] = 'Service update okpu:';
+$lang['clearcenter_subscription_update_complete'] = 'Ndenye aha update zuru ezu';
+$lang['clearcenter_system_not_registered'] = 'System na-adịghị aha.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Enweghị ike ịkpọtụrụ ime ihe nkesa.';
