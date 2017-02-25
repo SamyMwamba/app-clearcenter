86fcc5e37f55e2dc7a6a97fb0c2a55eb51796d24
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:25:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'tayanch tizimi ClearCenter dan dasturiy uchun vositalar o&#39;rnatilgan yadro beradi.';
+$lang['clearcenter_app_name'] = 'ClearCenter base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter ulash ...';
+$lang['clearcenter_connection_failed:'] = 'Ulanish muvaffaqiyatsiz tugadi:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS topilmadi.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'masofaviy tarmoq axborot mavjud emas.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Foydalanuvchi litsenziyalashtirish Qolgan:';
+$lang['clearcenter_service_is_up_to_date'] = 'Xizmat hozirgi hisoblanadi.';
+$lang['clearcenter_service_requires_an_update'] = 'Xizmat yangilanishini talab qiladi.';
+$lang['clearcenter_service_update_failed:'] = 'Xizmat yangilash muvaffaqiyatsiz tugadi:';
+$lang['clearcenter_subscription_update_complete'] = 'Obuna yangilash to&#39;liq';
+$lang['clearcenter_system_not_registered'] = 'System ro&#39;yxatga emas.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'masofaviy Serverga ulanib bo&#39;lmadi.';
