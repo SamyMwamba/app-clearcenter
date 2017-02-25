38db58052046d635a1e12c63aee4d15ce0ce2ec7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:25:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Mae&#39;r system sylfaen yn darparu craidd set o offer ar gyfer meddalwedd o ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Sylfaen';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Cysylltu â ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Methodd Cysylltiad:';
+$lang['clearcenter_dns_lookup_failed'] = 'Methodd am-edrych DNS.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Nid yw gwybodaeth o rhwydwaith o bell ar gael.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Weddill Trwyddedau Defnyddiwr:';
+$lang['clearcenter_service_is_up_to_date'] = 'Gwasanaeth i fyny-i-hyd yn hyn.';
+$lang['clearcenter_service_requires_an_update'] = 'yn gofyn am y wybodaeth ddiweddaraf gwasanaeth.';
+$lang['clearcenter_service_update_failed:'] = 'Wedi methu diweddaru&#39;r gwasanaeth:';
+$lang['clearcenter_subscription_update_complete'] = 'Diweddariad Tanysgrifiad cyflawn';
+$lang['clearcenter_system_not_registered'] = 'Nid yw system yn cael ei gofrestru.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Methu cysylltu â gweinydd pell.';
