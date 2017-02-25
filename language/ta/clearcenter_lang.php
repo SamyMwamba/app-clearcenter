6527721b97270074e862080d0043925ec4ce8c89
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:24:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'அடிப்படை கணினி ClearCenter இருந்து மென்பொருள் கருவிகள் அமைக்க ஒரு முக்கிய வழங்குகிறது.';
+$lang['clearcenter_app_name'] = 'ClearCenter பேஸ்';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter இணைக்கிறது ...';
+$lang['clearcenter_connection_failed:'] = 'இணைப்பு தோல்வியடைந்தது:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS தேடுதல் தோல்வி.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'தொலைதூர வலையமைப்பிற்கு இருந்து தகவல் கிடைக்கவில்லை.';
+$lang['clearcenter_remaining_user_licenses:'] = 'பயனர் உரிமங்களை மீதமுள்ள:';
+$lang['clearcenter_service_is_up_to_date'] = 'சேவை வரை தேதி ஆகும்.';
+$lang['clearcenter_service_requires_an_update'] = 'சேவை ஒரு புதுப்பிப்பு தேவைப்படுகிறது.';
+$lang['clearcenter_service_update_failed:'] = 'சேவை புதுப்பித்தல் தோல்வியடைந்தது:';
+$lang['clearcenter_subscription_update_complete'] = 'சந்தா புதுப்பிப்பு முழுமையான';
+$lang['clearcenter_system_not_registered'] = 'கணினி பதிவு செய்யப்படவில்லை.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'தொலை சர்வர் தொடர்பு கொள்ள முடியவில்லை.';
