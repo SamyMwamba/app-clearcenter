53eebe77b32469784674f688c86cff0daf38292a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:23:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Sistim basa nyadiakeun inti nangtukeun pakakas pikeun software ti ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Nyambungkeun ka ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Sambungan gagal:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS lookup gagal.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Informasi ti jaringan jauh teu sadia.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Sésana Pamaké lisensi:';
+$lang['clearcenter_service_is_up_to_date'] = 'Service nyaéta up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Service merlukeun hiji update.';
+$lang['clearcenter_service_update_failed:'] = 'update Service gagal:';
+$lang['clearcenter_subscription_update_complete'] = 'update langganan lengkep';
+$lang['clearcenter_system_not_registered'] = 'System teu kadaptar.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Teu bisa kontak server jauh.';
