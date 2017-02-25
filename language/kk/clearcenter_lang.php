68bc5c3a755d9c19a23d3fed2e4936f5819cbdac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:18:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'базалық жүйесі ClearCenter бағдарламалық құралдарының негізгі жиынтығын ұсынады.';
+$lang['clearcenter_app_name'] = 'ClearCenter Негізгі';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter қосылу ...';
+$lang['clearcenter_connection_failed:'] = 'Қосылым жаңылысы:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS іздеу сәтсіз аяқталды.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'қашықтан желіден ақпарат қол жетімді емес.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Қалған Пайдаланушы Лицензиялар:';
+$lang['clearcenter_service_is_up_to_date'] = 'Қызмет-ақ күні болып табылады.';
+$lang['clearcenter_service_requires_an_update'] = 'Қызмет жаңартуды талап етеді.';
+$lang['clearcenter_service_update_failed:'] = 'Қызмет жаңарту жаңылысы:';
+$lang['clearcenter_subscription_update_complete'] = 'толық жазылу жаңарту';
+$lang['clearcenter_system_not_registered'] = 'Жүйе тіркелмеген.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Қашықтағы серверіне қосылу мүмкін болмады.';
