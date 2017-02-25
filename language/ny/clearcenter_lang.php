0ac3e5698bdacf9cbb4cc98a6559f0fdfe2b94fb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:14:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'The dongosolo m&#39;munsi imapereka maziko anapereka zipangizo kuti mapulogalamu kuchokera ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Wolumikiza ku ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Kugwirizana analephera:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS osakira analephera.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Information kwa maukonde kutali palibe.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Otsala malaisensi Wosuta:';
+$lang['clearcenter_service_is_up_to_date'] = 'Service ndi mmwamba-ndi-tsiku.';
+$lang['clearcenter_service_requires_an_update'] = 'Service ungabwere pomwe.';
+$lang['clearcenter_service_update_failed:'] = 'Service pomwe analephera:';
+$lang['clearcenter_subscription_update_complete'] = 'Muzimvetsera pomwe wathunthu';
+$lang['clearcenter_system_not_registered'] = 'System si m&#39;kaundula.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Sanathe kulankhula ndi akumidzi Seva.';
