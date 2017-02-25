e4e478c165a6fa4f8d6130b59b9b932806504084
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:15:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Το βασικό σύστημα παρέχει ένα βασικό σετ εργαλείων για το λογισμικό από ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Βάση';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Σύνδεση με ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Αποτυχημένη σύνδεση:';
+$lang['clearcenter_dns_lookup_failed'] = 'αναζήτηση DNS απέτυχε.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Πληροφορίες από απομακρυσμένο δίκτυο δεν είναι διαθέσιμο.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Παραμένοντας άδειες User:';
+$lang['clearcenter_service_is_up_to_date'] = 'Η υπηρεσία είναι up-to-ημερομηνία.';
+$lang['clearcenter_service_requires_an_update'] = 'Υπηρεσία απαιτεί ενημέρωση.';
+$lang['clearcenter_service_update_failed:'] = 'Update Service απέτυχε:';
+$lang['clearcenter_subscription_update_complete'] = 'ενημέρωση συνδρομή πλήρη';
+$lang['clearcenter_system_not_registered'] = 'Το σύστημα δεν έχει καταχωρηθεί.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Αδύνατη η επικοινωνία με απομακρυσμένο διακομιστή.';
