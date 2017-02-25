b1d619a8ee5e20718234b61b03c031003f22e0b6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:24:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Ang batayang sistema ay nagbibigay ng isang pangunahing hanay ng mga kasangkapan para sa software mula ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Kumokonekta sa ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Nabigo Koneksyon:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS lookup nabigo.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Impormasyon mula sa remote network ay hindi magagamit.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Natitirang Lisensya User:';
+$lang['clearcenter_service_is_up_to_date'] = 'Service ay up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Service ay nangangailangan ng isang update.';
+$lang['clearcenter_service_update_failed:'] = 'Nabigo Service update:';
+$lang['clearcenter_subscription_update_complete'] = 'update Subscription kumpletong';
+$lang['clearcenter_system_not_registered'] = 'System ay hindi nakarehistro.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Hindi magawang makipag-ugnayan sa remote server.';
