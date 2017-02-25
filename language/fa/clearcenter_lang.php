cd3c3cc80503e4b464ffb40aa833bb58e8f500bd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:21:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'سیستم پایه یک هسته مجموعه ای از ابزار برای نرم افزار از ClearCenter فراهم می کند.';
+$lang['clearcenter_app_name'] = 'ClearCenter پایه';
+$lang['clearcenter_connecting_to_clearcenter'] = 'اتصال به ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'ارتباط ناموفق بود:';
+$lang['clearcenter_dns_lookup_failed'] = 'جستجوی DNS انجام نشد.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'اطلاعات از راه دور شبکه در دسترس نیست.';
+$lang['clearcenter_remaining_user_licenses:'] = 'باقی مانده مجوز کاربر:';
+$lang['clearcenter_service_is_up_to_date'] = 'خدمات است تا به تاریخ.';
+$lang['clearcenter_service_requires_an_update'] = 'سرویس نیاز به به روز رسانی.';
+$lang['clearcenter_service_update_failed:'] = 'به روز رسانی خدمات شکست خورده:';
+$lang['clearcenter_subscription_update_complete'] = 'به روز رسانی اشتراک کامل';
+$lang['clearcenter_system_not_registered'] = 'سیستم ثبت نشده است.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'قادر به تماس با سرور از راه دور.';
