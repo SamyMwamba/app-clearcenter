38a68a8707b48d3dea452e20d760f17c375846ae
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:18:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'ბაზის სისტემა უზრუნველყოფს ძირითადი კომპლექტი ინსტრუმენტები პროგრამული უზრუნველყოფა ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter ბაზა';
+$lang['clearcenter_connecting_to_clearcenter'] = 'დამაკავშირებელი ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'კავშირი უარყოფილია:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS lookup ვერ მოხერხდა.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'ინფორმაცია დისტანციური ქსელი არ არის ხელმისაწვდომი.';
+$lang['clearcenter_remaining_user_licenses:'] = 'დარჩენილი მომხმარებელი ლიცენზიები:';
+$lang['clearcenter_service_is_up_to_date'] = 'სამსახურის up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'სამსახურის მოითხოვს განახლება.';
+$lang['clearcenter_service_update_failed:'] = 'სამსახურის განახლება ვერ მოხერხდა:';
+$lang['clearcenter_subscription_update_complete'] = 'გამოწერა განახლება დასრულდა';
+$lang['clearcenter_system_not_registered'] = 'სისტემა არ არის რეგისტრირებული.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'ვერ ვუკავშირდები სერვერზე.';
