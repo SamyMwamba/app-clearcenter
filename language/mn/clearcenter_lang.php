6e600920ba2679856e67160110002cfb490fa34e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:20:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'үндсэн систем ClearCenter нь програм хангамж, хэрэгслийг тогтоосон цөм болно.';
+$lang['clearcenter_app_name'] = 'ClearCenter үндсэн';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter холбогдож байна ...';
+$lang['clearcenter_connection_failed:'] = 'Холболт амжилтгүй болсон:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS хайлт амжилтгүй болсон.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'алсын сүлжээнээс мэдээлэл байхгүй байна.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Хэрэглэгчийн лиценз Үлдсэн:';
+$lang['clearcenter_service_is_up_to_date'] = 'Үйлчилгээ дээр үеийн юм.';
+$lang['clearcenter_service_requires_an_update'] = 'Үйлчилгээ шинэчлэлтийг шаарддаг.';
+$lang['clearcenter_service_update_failed:'] = 'Үйлчилгээ шинэчлэлт амжилтгүй:';
+$lang['clearcenter_subscription_update_complete'] = 'Захиалгын шинэчлэх иж бүрэн';
+$lang['clearcenter_system_not_registered'] = 'Системийн байгааг бүртгээгүй байна.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'алсын сервер холбогдох боломжгүй байна.';
