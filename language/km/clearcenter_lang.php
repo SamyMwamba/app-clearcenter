cecc4e9e11343ec790779a6dabf6e07bdcabeb94
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:18:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'ប្រព័ន្ធគោលនេះបានផ្តល់នូវស្នូលកំណត់ឧបករណ៍សម្រាប់កម្មវិធីពី ClearCenter មួយ។';
+$lang['clearcenter_app_name'] = 'ClearCenter មូលដ្ឋាន';
+$lang['clearcenter_connecting_to_clearcenter'] = 'តភ្ជាប់ទៅ ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'ការ​ត​ភ្ជាប់​បាន​បរាជ័យ:';
+$lang['clearcenter_dns_lookup_failed'] = 'រកមើលឈ្មោះ DNS បរាជ័យ។';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'ពពីបណ្តាញពីចម្ងាយគឺមិនអាចរកបាន។';
+$lang['clearcenter_remaining_user_licenses:'] = 'នៅសល់អាជ្ញាប័ណ្ណអ្នកប្រើប្រាស់:';
+$lang['clearcenter_service_is_up_to_date'] = 'សេវាគឺមានរហូតដល់កាលបរិច្ឆេទ។';
+$lang['clearcenter_service_requires_an_update'] = 'សេវាតម្រូវឱ្យមានការធ្វើឱ្យទាន់សម័យមួយ។';
+$lang['clearcenter_service_update_failed:'] = 'ធ្វើឱ្យទាន់សម័យសេវាបរាជ័យ:';
+$lang['clearcenter_subscription_update_complete'] = 'ធ្វើឱ្យទាន់សម័យជាវបានបញ្ចប់';
+$lang['clearcenter_system_not_registered'] = 'ប្រព័ន្ធមួយត្រូវបានមិនបានចុះឈ្មោះ។';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'មិនអាចទាក់ទងម៉ាស៊ីនបម្រើពីចម្ងាយ។';
