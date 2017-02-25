94d883cb6d0445d546fb22b6a281bfdd116e2adf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:25:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'بیس سسٹم ClearCenter کے سے سافٹ ویئر کے لئے ٹولز کا مجموعہ ایک کور فراہم کرتا ہے.';
+$lang['clearcenter_app_name'] = 'ClearCenter کے بیس';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter کے سے متصل ہونا ...';
+$lang['clearcenter_connection_failed:'] = 'رابطہ ناکام:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS تلاش میں ناکام رہے.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'ریموٹ نیٹ ورک سے معلومات دستیاب نہیں ہے.';
+$lang['clearcenter_remaining_user_licenses:'] = 'صارف لائسنس باقی:';
+$lang['clearcenter_service_is_up_to_date'] = 'سروس کو اپ ڈیٹ کرنے کے لئے ہے.';
+$lang['clearcenter_service_requires_an_update'] = 'سروس ایک اپ ڈیٹ کی ضرورت ہے.';
+$lang['clearcenter_service_update_failed:'] = 'سروس ید ناکام ہو گئی:';
+$lang['clearcenter_subscription_update_complete'] = 'سکریپشن اپ ڈیٹ مکمل';
+$lang['clearcenter_system_not_registered'] = 'سسٹم رجسٹرڈ نہیں ہے.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'ریموٹ سرور سے رابطہ کرنے سے قاصر ہے.';
