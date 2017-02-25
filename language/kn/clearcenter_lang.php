1fc4280cd5db5a37f6455ec971c43566a8c8010e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:18:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'ಮೂಲ ವ್ಯವಸ್ಥೆ ClearCenter ಸಾಫ್ಟ್ವೇರ್ ಸಾಧನಗಳನ್ನು ಸೆಟ್ ಒಂದು ಕೋರ್ ಒದಗಿಸುತ್ತದೆ.';
+$lang['clearcenter_app_name'] = 'ClearCenter ಬೇಸ್';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter ಸಂಪರ್ಕಿಸಲಾಗುತ್ತಿದೆ ...';
+$lang['clearcenter_connection_failed:'] = 'ಸಂಪರ್ಕ ವಿಫಲವಾಗಿದೆ:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS ಲುಕಪ್ ವಿಫಲವಾಗಿದೆ.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'ದೂರಸ್ಥ ನೆಟ್ವರ್ಕ್ನಿಂದ ಮಾಹಿತಿಯನ್ನು ಲಭ್ಯವಿಲ್ಲ.';
+$lang['clearcenter_remaining_user_licenses:'] = 'ಉಳಿದ ಬಳಕೆದಾರ ಪರವಾನಗಿಗಳು:';
+$lang['clearcenter_service_is_up_to_date'] = 'ಸೇವೆ ಅಪ್ ಯಾ ದಿನಾಂಕ.';
+$lang['clearcenter_service_requires_an_update'] = 'ಸೇವೆ ನವೀಕರಣದ ಅಗತ್ಯವಿದೆ.';
+$lang['clearcenter_service_update_failed:'] = 'ಸೇವೆ ನವೀಕರಣ ವಿಫಲವಾಗಿದೆ:';
+$lang['clearcenter_subscription_update_complete'] = 'ಚಂದಾದಾರಿಕೆ ನವೀಕರಣ ಪೂರ್ಣಗೊಂಡಿದೆ';
+$lang['clearcenter_system_not_registered'] = 'ವ್ಯವಸ್ಥೆ ನೋಂದಣಿಯಾಗಿಲ್ಲ.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'ದೂರಸ್ಥ ಸರ್ವರ್ ಸಂಪರ್ಕಿಸಲು ಸಾಧ್ಯವಿಲ್ಲ.';
