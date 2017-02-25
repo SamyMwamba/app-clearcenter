7de1ba7d36e89c075eeeb9612dd7f8cdec45a7dc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:12:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'وينص النظام الأساسي مجموعة أساسية من أدوات للبرمجيات من ClearCenter.';
+$lang['clearcenter_app_name'] = 'قاعدة ClearCenter';
+$lang['clearcenter_connecting_to_clearcenter'] = 'الاتصال ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'فشل الاتصال:';
+$lang['clearcenter_dns_lookup_failed'] = 'فشل بحث DNS.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'معلومات من شبكة الاتصال البعيدة غير متوفرة.';
+$lang['clearcenter_remaining_user_licenses:'] = 'تبقى تراخيص العضو:';
+$lang['clearcenter_service_is_up_to_date'] = 'خدمة ما يصل إلى التاريخ.';
+$lang['clearcenter_service_requires_an_update'] = 'تتطلب خدمة التحديث.';
+$lang['clearcenter_service_update_failed:'] = 'تحديث خدمة فشل:';
+$lang['clearcenter_subscription_update_complete'] = 'تحديث الاشتراك كاملة';
+$lang['clearcenter_system_not_registered'] = 'لم يتم تسجيل النظام.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'غير قادر على الاتصال الملقم البعيد.';
