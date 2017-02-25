c5f2c0ea5d2b77e3a93ed514635df4ec7f46f20e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:13:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'বেস সিস্টেমের একটি কোর ClearCenter থেকে সফ্টওয়্যার জন্য সরঞ্জামের সেট প্রদান করে.';
+$lang['clearcenter_app_name'] = 'ClearCenter বেজ';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter সংযুক্ত হচ্ছে ...';
+$lang['clearcenter_connection_failed:'] = 'সংযোগ ব্যর্থ হয়েছে:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS খোঁজ ব্যর্থ হয়েছে.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'দূরবর্তী নেটওয়ার্ক থেকে তথ্য পাওয়া যায় না.';
+$lang['clearcenter_remaining_user_licenses:'] = 'ব্যবহারকারী লাইসেন্স অবশিষ্ট:';
+$lang['clearcenter_service_is_up_to_date'] = 'সেবা আপ-টু-ডেট হয়.';
+$lang['clearcenter_service_requires_an_update'] = 'সার্ভিস একটি আপডেটের প্রয়োজন.';
+$lang['clearcenter_service_update_failed:'] = 'পরিষেবা আপডেট ব্যর্থ হয়েছে:';
+$lang['clearcenter_subscription_update_complete'] = 'সাবস্ক্রিপশন আপডেট সম্পূর্ণ';
+$lang['clearcenter_system_not_registered'] = 'সিস্টেম নিবন্ধিত করা হয় না.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'দূরবর্তী সার্ভারের সাথে যোগাযোগ করতে অক্ষম.';
