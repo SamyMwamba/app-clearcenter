de63472d3ed7e12cab26750aa407f421546e98fc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:24:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'బేస్ సిస్టమ్ ClearCenter నుండి సాఫ్ట్వేర్ సాధనాల సెట్ ఒక కోర్ అందిస్తుంది.';
+$lang['clearcenter_app_name'] = 'ClearCenter బేస్';
+$lang['clearcenter_connecting_to_clearcenter'] = 'కు ClearCenter కనెక్ట్ అవుతోంది ...';
+$lang['clearcenter_connection_failed:'] = 'సంధానము విఫలమైనది:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS శోధన విఫలమైంది.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'రిమోట్ నెట్వర్క్ లో ఉన్న వివరణని అందుబాటులో లేదు.';
+$lang['clearcenter_remaining_user_licenses:'] = 'వాడుకరి లైసెన్స్ మిగిలి:';
+$lang['clearcenter_service_is_up_to_date'] = 'సర్వీస్ను నవీనమైన ఉంది.';
+$lang['clearcenter_service_requires_an_update'] = 'సర్వీస్ నవీకరణ అవసరం.';
+$lang['clearcenter_service_update_failed:'] = 'సేవా నవీకరణ విఫలమైంది:';
+$lang['clearcenter_subscription_update_complete'] = 'సభ్యత్వ నవీకరణ పూర్తయినప్పుడు';
+$lang['clearcenter_system_not_registered'] = 'వ్యవస్థ నమోదు కాలేదు.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'రిమోట్ సర్వర్ సంప్రదించడం సాధ్యం కాదు.';
