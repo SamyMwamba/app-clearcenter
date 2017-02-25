9ab192f2924706e26eddfa68bcf5c3ad9d595faa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:14:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = '基本系统提供了一个核心从ClearCenter软件的工具集。';
+$lang['clearcenter_app_name'] = 'ClearCenter基地';
+$lang['clearcenter_connecting_to_clearcenter'] = '连接到ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = '连接失败：';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS查找失败。';
+$lang['clearcenter_information_from_remote_network_not_available'] = '从远程网络的信息不可用。';
+$lang['clearcenter_remaining_user_licenses:'] = '剩余用户许可：';
+$lang['clearcenter_service_is_up_to_date'] = '服务是最新最新的。';
+$lang['clearcenter_service_requires_an_update'] = '服务需要更新。';
+$lang['clearcenter_service_update_failed:'] = '服务更新失败：';
+$lang['clearcenter_subscription_update_complete'] = '订阅更新完成';
+$lang['clearcenter_system_not_registered'] = '系统未注册。';
+$lang['clearcenter_unable_to_contact_remote_server'] = '无法联系远程服务器。';
