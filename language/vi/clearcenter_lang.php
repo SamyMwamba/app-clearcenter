b37294f77a6fea79e87239ccec62636fd6d78dff
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:25:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Hệ thống cơ sở cung cấp một lõi bộ công cụ cho phần mềm từ ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter cơ sở';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Kết nối đến ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Kết nối thất bại:';
+$lang['clearcenter_dns_lookup_failed'] = 'tra cứu DNS thất bại.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Thông tin từ xa qua mạng là không có sẵn.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Còn lại giấy phép người dùng:';
+$lang['clearcenter_service_is_up_to_date'] = 'Dịch vụ là up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Dịch vụ yêu cầu cập nhật.';
+$lang['clearcenter_service_update_failed:'] = 'cập nhật dịch vụ thất bại:';
+$lang['clearcenter_subscription_update_complete'] = 'cập nhật đăng ký hoàn chỉnh';
+$lang['clearcenter_system_not_registered'] = 'Hệ thống không được đăng ký.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Không thể liên lạc với máy chủ từ xa.';
