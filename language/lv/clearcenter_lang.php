de8f5ec26feeb0778efe89c6c57fbd3c25c88665
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:19:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Bāzes sistēma nodrošina kopums, rīku programmatūru no ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Pieslēgšanās ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Savienojums neizdevās:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS uzmeklēšana neizdevās.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Informācija no tālvadības tīkls nav pieejams.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Atlikušais lietotāju licencēm:';
+$lang['clearcenter_service_is_up_to_date'] = 'Pakalpojums ir up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Apkalpošana prasa atjauninājumu.';
+$lang['clearcenter_service_update_failed:'] = 'Pakalpojumu atjaunināšana neizdevās:';
+$lang['clearcenter_subscription_update_complete'] = 'Abonēšanas atjaunināšana pabeigta';
+$lang['clearcenter_system_not_registered'] = 'Sistēma nav reģistrēta.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Nevar sazināties ar attālo serveri.';
