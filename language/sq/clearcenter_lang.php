f6c39676f26f4610918099730ebcfdc3bd30ef24
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:12:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Sistemi bazë ofron një bazë të caktuar të mjeteve për software nga ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Po lidhet me ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Lidhja dështoi:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS lookup dështuar.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Informacione nga rrjeti largët nuk është në dispozicion.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Mbetur licencat e përdoruesit:';
+$lang['clearcenter_service_is_up_to_date'] = 'Shërbimi është up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Shërbimit kërkon një update.';
+$lang['clearcenter_service_update_failed:'] = 'Përditësimi shërbimit dështoi:';
+$lang['clearcenter_subscription_update_complete'] = 'Përditësimi Abonimi plotë';
+$lang['clearcenter_system_not_registered'] = 'Sistemi nuk është i regjistruar.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Në pamundësi për të kontaktuar server të largët.';
