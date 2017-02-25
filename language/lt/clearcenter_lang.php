75712a5aeff72224f41683ca79c553195aad3726
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:19:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Bazinė sistema suteikia branduolį įrankių rinkinys už programinę įrangą iš ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter bazė';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Prijungimas prie ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Nepavyko prisijungti:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS nepavyko.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Informacija iš nuotolinio tinklo nėra.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Likęs vartotojų licencijos:';
+$lang['clearcenter_service_is_up_to_date'] = 'Paslauga yra up-to-data.';
+$lang['clearcenter_service_requires_an_update'] = 'Paslaugų reikia atnaujinti.';
+$lang['clearcenter_service_update_failed:'] = 'Paslaugų naujinimas nepavyko:';
+$lang['clearcenter_subscription_update_complete'] = 'Prenumerata atnaujinimas baigtas';
+$lang['clearcenter_system_not_registered'] = 'Sistema nėra registruotas.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Nepavyksta susisiekti su nuotolinio serverio.';
