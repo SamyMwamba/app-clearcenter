ab5d322e5ff207e445ce566455ed696ef2aabed6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:16:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'आधार प्रणाली एक कोर ClearCenter से सॉफ्टवेयर के लिए उपकरणों का सेट प्रदान करता है।';
+$lang['clearcenter_app_name'] = 'ClearCenter बेस';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter करने के लिए कनेक्ट ...';
+$lang['clearcenter_connection_failed:'] = 'कनेक्शन विफल:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS लुकअप विफल रहा है।';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'दूरदराज के नेटवर्क से जानकारी उपलब्ध नहीं है।';
+$lang['clearcenter_remaining_user_licenses:'] = 'उपयोगकर्ता लाइसेंस शेष:';
+$lang['clearcenter_service_is_up_to_date'] = 'सेवा अप-टू-डेट है।';
+$lang['clearcenter_service_requires_an_update'] = 'सेवा के लिए एक अद्यतन की आवश्यकता है।';
+$lang['clearcenter_service_update_failed:'] = 'सेवा अद्यतन विफल:';
+$lang['clearcenter_subscription_update_complete'] = 'सदस्यता अद्यतन पूर्ण';
+$lang['clearcenter_system_not_registered'] = 'प्रणाली में पंजीकृत नहीं है।';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'दूरदराज के सर्वर से संपर्क करने में असमर्थ है।';
