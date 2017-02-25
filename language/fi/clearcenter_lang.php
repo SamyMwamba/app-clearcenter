c6b57ad72685e8059458feb69887fc6ea1d32536
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:15:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Pohja tarjoaa ydin joukko työkaluja ohjelmiston ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Yhteyden ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Yhteys epäonnistui:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS-haku epäonnistui.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Tiedot kauko-verkko ei ole käytettävissä.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Jäljellä käyttäjän käyttöoikeudet:';
+$lang['clearcenter_service_is_up_to_date'] = 'Palvelu on ajan tasalla.';
+$lang['clearcenter_service_requires_an_update'] = 'Palvelu vaatii päivitystä.';
+$lang['clearcenter_service_update_failed:'] = 'Palvelu päivitys epäonnistui:';
+$lang['clearcenter_subscription_update_complete'] = 'Tilaus päivitys valmis';
+$lang['clearcenter_system_not_registered'] = 'Järjestelmä ei ole rekisteröity.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Ei yhteyttä etäpalvelimeen.';
