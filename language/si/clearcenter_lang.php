30ecf7bf571c76c7abc85b24ed8464fa380b5c31
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:22:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'මූල පද්ධතිය ClearCenter මෘදුකාංග සඳහා වන මෙවලම් මූලික මාලාවක් සපයයි.';
+$lang['clearcenter_app_name'] = 'ClearCenter මූලික';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter සම්බන්ධ ...';
+$lang['clearcenter_connection_failed:'] = 'සම්බන්ධතාව අසමත් විය:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS බැලීමට නොහැකි විය.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'දුරස්ථ ජාල තොරතුරු ලබාගත නොහැක.';
+$lang['clearcenter_remaining_user_licenses:'] = 'පරිශීලක බලපත්ර ඉතිරි:';
+$lang['clearcenter_service_is_up_to_date'] = 'සේවා යාවත්කාලීන වේ.';
+$lang['clearcenter_service_requires_an_update'] = 'සේවා යාවත්කාලීන අවශ්ය වේ.';
+$lang['clearcenter_service_update_failed:'] = 'සේවා යාවත්කාලීන අසමත් විය:';
+$lang['clearcenter_subscription_update_complete'] = 'දායකත්ව යාවත් කාලීන වීම සම්පුර්ණ';
+$lang['clearcenter_system_not_registered'] = 'පද්ධතිය ලියාපදිංචි කර නොමැත.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'දුරස්ථ සේවාදායකය හා සම්බන්ධ වීමට නොහැකි විය.';
