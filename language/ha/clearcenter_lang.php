6a11b2ae342df2aa4ac597d9458b03bc7b873aa0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:16:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'A tushe tsarin samar da wani core kafa na kayayyakin aiki, don software daga ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Tushe';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Haɗa zuwa ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Connection kasa:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS nemo kasa.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Bayani daga m cibiyar sadarwa ba available.';
+$lang['clearcenter_remaining_user_licenses:'] = 'M User Lasisi:';
+$lang['clearcenter_service_is_up_to_date'] = 'Service ne up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Service bukatar wani update.';
+$lang['clearcenter_service_update_failed:'] = 'Service update kasa:';
+$lang['clearcenter_subscription_update_complete'] = 'Subscription karshe complete';
+$lang['clearcenter_system_not_registered'] = 'System ba rajista.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Kasa a tuntube m uwar garken.';
