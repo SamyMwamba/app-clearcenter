fa5ffe8bd1560976efa91e9c4e714d161d3c1250
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:23:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'mfumo wa msingi hutoa msingi seti ya zana kwa ajili ya programu kutoka ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Msingi';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Kuunganisha na ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Connection alishindwa:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS ulishindikana.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Habari kutoka mtandao wa kijijini haipatikani.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Iliyobaki Leseni mtumiaji:';
+$lang['clearcenter_service_is_up_to_date'] = 'Huduma ni up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Huduma inahitaji update.';
+$lang['clearcenter_service_update_failed:'] = 'Huduma update alishindwa:';
+$lang['clearcenter_subscription_update_complete'] = 'Michango update kamili';
+$lang['clearcenter_system_not_registered'] = 'System haijasajiliwa.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Haiwezekani kuwasiliana kijijini server.';
