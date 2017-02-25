efa93ce7889ef332ee7bba23c7957e9648e36804
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:25:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'די באַזע סיסטעם גיט אַ האַרץ שטעלן פון מכשירים פֿאַר ווייכווארג פֿון קלעאַרסענטער.';
+$lang['clearcenter_app_name'] = 'קלעאַרסענטער באַזע';
+$lang['clearcenter_connecting_to_clearcenter'] = 'קאַנעקטינג צו קלעאַרסענטער ...';
+$lang['clearcenter_connection_failed:'] = 'קאַנעקשאַן אַנדערש:';
+$lang['clearcenter_dns_lookup_failed'] = 'דנס לאָאָקופּ אַנדערש.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'אינפֿאָרמאַציע פון ​​ווייַט נעץ איז ניט בנימצא.';
+$lang['clearcenter_remaining_user_licenses:'] = 'רוען באַניצער ליסענסעס:';
+$lang['clearcenter_service_is_up_to_date'] = 'סערוויס איז אַרויף-צו-דאַטע.';
+$lang['clearcenter_service_requires_an_update'] = 'א ריקווייערז אַ דערהייַנטיקן.';
+$lang['clearcenter_service_update_failed:'] = 'א דערהייַנטיקן אַנדערש:';
+$lang['clearcenter_subscription_update_complete'] = 'אַבאָנעמענט דערהייַנטיקן גאַנץ';
+$lang['clearcenter_system_not_registered'] = 'סיסטעם איז נישט רעגיסטרירט.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'געקענט צו קאָנטאַקט ווייַט סערווער.';
