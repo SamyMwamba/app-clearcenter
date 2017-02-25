4e2568975f8df2db7e829dcb90acebdd1f946bfe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:24:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Temel sistem ClearCenter gelen yazılım için bir dizi araçları çekirdek sağlar.';
+$lang['clearcenter_app_name'] = 'ClearCenter Bankası';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter bağlanıyor ...';
+$lang['clearcenter_connection_failed:'] = 'Bağlantı başarısız oldu:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS araması başarısız oldu.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'uzak ağdan bilgi mevcut değildir.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Kullanıcı Lisansları Kalan:';
+$lang['clearcenter_service_is_up_to_date'] = 'Hizmet-to-date olduğunu.';
+$lang['clearcenter_service_requires_an_update'] = 'Hizmet bir güncelleştirme gerektirir.';
+$lang['clearcenter_service_update_failed:'] = 'Servis güncellemesi başarısız oldu:';
+$lang['clearcenter_subscription_update_complete'] = 'Abonelik güncelleme tamamlandı';
+$lang['clearcenter_system_not_registered'] = 'Sistem kayıtlı değil.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Uzak sunucuyla bağlantı kurulamıyor.';
