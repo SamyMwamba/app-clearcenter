02bc4d262d97dbd37b364837e1413826766475da
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:21:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'ਬੇਸ ਸਿਸਟਮ ਨੂੰ ਇੱਕ ਕੋਰ ClearCenter ਤੱਕ ਸਾਫਟਵੇਅਰ ਲਈ ਟੂਲ ਦੀ ਸੈੱਟ ਦਿੰਦਾ ਹੈ.';
+$lang['clearcenter_app_name'] = 'ClearCenter ਬੇਸ';
+$lang['clearcenter_connecting_to_clearcenter'] = 'ClearCenter ਨਾਲ ਕਨੈਕਟ ...';
+$lang['clearcenter_connection_failed:'] = 'ਕੁਨੈਕਸ਼ਨ ਫੇਲ੍ਹ ਹੈ:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS ਦੀ ਖੋਜ ਫੇਲ੍ਹ ਹੈ.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'ਰਿਮੋਟ ਨੈੱਟਵਰਕ ਜਾਣਕਾਰੀ ਉਪਲਬਧ ਨਹੀ ਹੈ.';
+$lang['clearcenter_remaining_user_licenses:'] = 'ਯੂਜ਼ਰ ਲਾਇਸੰਸ ਬਾਕੀ:';
+$lang['clearcenter_service_is_up_to_date'] = 'ਸਰਵਿਸ ਅਪ-ਟੂ-ਦੀ ਮਿਤੀ ਹੈ.';
+$lang['clearcenter_service_requires_an_update'] = 'ਸਰਵਿਸ ਇੱਕ ਅੱਪਡੇਟ ਦੀ ਲੋੜ ਹੈ.';
+$lang['clearcenter_service_update_failed:'] = 'ਸੇਵਾ ਅੱਪਡੇਟ ਫੇਲ੍ਹ ਹੈ:';
+$lang['clearcenter_subscription_update_complete'] = 'ਗਾਹਕੀ ਅੱਪਡੇਟ ਮੁਕੰਮਲ ਹੋ';
+$lang['clearcenter_system_not_registered'] = 'ਸਿਸਟਮ ਰਜਿਸਟਰ ਨਾ ਗਿਆ ਹੈ.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'ਰਿਮੋਟ ਸਰਵਰ ਨਾਲ ਸੰਪਰਕ ਕਰਨ ਵਿੱਚ ਅਸਫਲ ਹੈ.';
