afe4aae219bad7a9d43efa439ad81d3bf78423d3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:17:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Soláthraíonn an córas bonn croí sraith uirlisí le haghaidh bogearraí ó ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Ag ceangal le ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Ceangal Theip:';
+$lang['clearcenter_dns_lookup_failed'] = 'Theip ar DNS Lookup.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Níl eolas ó líonra iargúlta ar fáil.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Fágtha Ceadúnais Úsáideoir:';
+$lang['clearcenter_service_is_up_to_date'] = 'Service cothrom le dáta.';
+$lang['clearcenter_service_requires_an_update'] = 'Éilíonn Seirbhís nuashonrú.';
+$lang['clearcenter_service_update_failed:'] = 'thabhairt cothrom le dáta Seirbhís Theip:';
+$lang['clearcenter_subscription_update_complete'] = 'Síntiúis nuashonrú iomlán';
+$lang['clearcenter_system_not_registered'] = 'Níl córas cláraithe.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Ní féidir dul i dteagmháil leis an bhfreastalaí iargúlta.';
