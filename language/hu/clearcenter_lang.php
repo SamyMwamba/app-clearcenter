f1679656f115139bbfb691994c1fa07f94f335a9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:16:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Az alap rendszer egy központi eszközök szoftver ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Csatlakozás ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Kapcsolat nem sikerült:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS-keresés nem sikerült.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Tájékoztatás a távoli hálózat nem elérhető.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Hátralévő felhasználói licencek:';
+$lang['clearcenter_service_is_up_to_date'] = 'Szolgáltatás up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Szolgáltatás frissítését igényli.';
+$lang['clearcenter_service_update_failed:'] = 'Szolgáltatás frissítés nem sikerült:';
+$lang['clearcenter_subscription_update_complete'] = 'Előfizetés frissítése befejeződött';
+$lang['clearcenter_system_not_registered'] = 'A rendszer nem regisztrált.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Nem sikerült kapcsolatba lépni a távoli szerverre.';
