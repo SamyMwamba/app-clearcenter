834274241b071d31f89cdf0d5b1a0d3a9b68b166
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:23:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Tsamaiso botlaaseng fana ka konokono beha ya disebediswa bakeng sa software e tswang ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Tlohang ho ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Mabapi hlōleha:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS Lookup hlōlehileng ho phethahala.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Tlhahisoleseding ho tswa netweke ka thōko ha e fumanehe.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Setseng dilaesense User:';
+$lang['clearcenter_service_is_up_to_date'] = 'Service ke ba morao-letsatsi.';
+$lang['clearcenter_service_requires_an_update'] = 'Service hloka apdeite.';
+$lang['clearcenter_service_update_failed:'] = 'Service update loi hlōleha:';
+$lang['clearcenter_subscription_update_complete'] = 'Tshebeletso ya update loi ka ho feletseng';
+$lang['clearcenter_system_not_registered'] = 'System e sa ngolisoa ka molao.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Ha ho kgonehe ho ikopanya ka thōko seva.';
