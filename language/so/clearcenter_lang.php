74504974da37d4c111109143d7feeb2a4a213893
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:23:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'nidaamka saldhigga wuxuu bixiyaa asaasiga a dhigay qalabka loogu talagalay software ka ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Ku xiridda in ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Connection ku guuldareystay:';
+$lang['clearcenter_dns_lookup_failed'] = 'Lookup DNS guuldareystay.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Warbixin ka network fog aan la heli karin.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Haray shattiyada User:';
+$lang['clearcenter_service_is_up_to_date'] = 'Adeegga waa kor-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Service u baahan update ah.';
+$lang['clearcenter_service_update_failed:'] = 'update Service guuldareystay:';
+$lang['clearcenter_subscription_update_complete'] = 'update Times dhameystiran';
+$lang['clearcenter_system_not_registered'] = 'System aan la diiwaan-galiyay.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Lama soo xiriir server fog.';
