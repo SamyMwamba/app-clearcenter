7a9c39252c6d84c4323781288d177c25b6d2f471
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:17:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Grunnurinn Kerfið veitir algerlega sett af verkfærum fyrir hugbúnað frá ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Tengist við ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Tenging mistókst:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS útlit mistókst.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Upplýsingar frá ytra neti er ekki í boði.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Eftir notandaleyfi:';
+$lang['clearcenter_service_is_up_to_date'] = 'Þjónusta er upp-til-dagsetning.';
+$lang['clearcenter_service_requires_an_update'] = 'Þjónusta krefst uppfærslu.';
+$lang['clearcenter_service_update_failed:'] = 'Þjónusta Uppfærsla mistókst:';
+$lang['clearcenter_subscription_update_complete'] = 'Áskrift uppfærslu lokið';
+$lang['clearcenter_system_not_registered'] = 'Kerfið er ekki skráð.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Ekki er hægt að hafa samband við fjarlægur framreiðslumaður.';
