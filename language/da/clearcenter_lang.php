ea9b434e192302c0f98d43a7360a2a15a6eb2393
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:14:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Basen system giver en kerne sæt af værktøjer til software fra ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Tilslutning til ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Forbindelsen mislykkedes:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS-opslag mislykkedes.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Oplysninger fra fjerntliggende netværk ikke er tilgængeligt.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Resterende brugerlicenser:';
+$lang['clearcenter_service_is_up_to_date'] = 'Service er up-to-date.';
+$lang['clearcenter_service_requires_an_update'] = 'Tjenesten kræver en opdatering.';
+$lang['clearcenter_service_update_failed:'] = 'Tjenesten opdatering mislykkedes:';
+$lang['clearcenter_subscription_update_complete'] = 'Abonnement opdatering komplet';
+$lang['clearcenter_system_not_registered'] = 'Systemet er ikke registreret.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Kan ikke kontakte fjernserver.';
