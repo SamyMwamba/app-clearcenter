6dc94f57192c72bb99525cd3f0dbeea6650d7064
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:22:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Базовая система обеспечивает основной набор инструментов для программного обеспечения от ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Подключение к ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Не удалось установить соединение:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS поиска не удалось.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Информация от удаленной сети недоступна.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Оставаясь пользовательских лицензий:';
+$lang['clearcenter_service_is_up_to_date'] = 'Сервис до современных.';
+$lang['clearcenter_service_requires_an_update'] = 'Сервис требует обновления.';
+$lang['clearcenter_service_update_failed:'] = 'Служба обновления не удалось:';
+$lang['clearcenter_subscription_update_complete'] = 'Обновление подписки завершена';
+$lang['clearcenter_system_not_registered'] = 'Система не зарегистрирована.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Не удается связаться с удаленным сервером.';
