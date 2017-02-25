23166e970bad172c465a2e0f4d95f74e4f238e9c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:19:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'whakarato te pūnaha turanga he matua whakaturia o taputapu mō te pūmanawa i ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Hono ki ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'I rahua te Hononga:';
+$lang['clearcenter_dns_lookup_failed'] = 'I rahua te Rapu DNS.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'e kore e wātea ngā mōhiohio i te whatunga mamao.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Toe Raihana Kaiwhakamahi:';
+$lang['clearcenter_service_is_up_to_date'] = 'Ko runga-ki-te rā Ratonga.';
+$lang['clearcenter_service_requires_an_update'] = 'titau Ratonga te whakahou.';
+$lang['clearcenter_service_update_failed:'] = 'I rahua te whakahou Ratonga:';
+$lang['clearcenter_subscription_update_complete'] = 'oti whakahou Ohaurunga';
+$lang['clearcenter_system_not_registered'] = 'e kore te rēhita Pūnaha.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Kāore e taea te whakapā atu tūmau mamao.';
