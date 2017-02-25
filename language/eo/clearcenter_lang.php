d8bc82c516fecb532c5199e25f6d5d990463e185
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:14:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'La baza sistemo provizas kernan aro de iloj por softvaro de ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Bazo';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Konektante al ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Rilato malsukcesis:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS lookup malsukcesis.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Informo el foraj reto ne estas havebla.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Restanta Uzanto Licencoj:';
+$lang['clearcenter_service_is_up_to_date'] = 'Servo estas supre-ĝis-dato.';
+$lang['clearcenter_service_requires_an_update'] = 'Servo postulas ĝisdatigon.';
+$lang['clearcenter_service_update_failed:'] = 'Servo ĝisdatigo malsukcesis:';
+$lang['clearcenter_subscription_update_complete'] = 'Abono ĝisdatigo kompleta';
+$lang['clearcenter_system_not_registered'] = 'Sistemo ne estas registrita.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Nekapabla kontakti fora servilo.';
