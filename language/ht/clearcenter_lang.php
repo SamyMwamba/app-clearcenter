36ab80e19d2e0da8aa239ea0192bb88f5abe23fd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:16:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Sistèm nan baz bay yon nwayo mete nan zouti pou lojisyèl soti nan ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Sèvi';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Konekte ak lòt moun ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Koneksyon echwe:';
+$lang['clearcenter_dns_lookup_failed'] = 'Dns Passage echwe.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Enfòmasyon ki soti nan rezo aleka a pa disponib.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Rete Itilizatè Lisans:';
+$lang['clearcenter_service_is_up_to_date'] = 'Sèvis se monte-a-dat.';
+$lang['clearcenter_service_requires_an_update'] = 'Lapòs mande pou yon ajou.';
+$lang['clearcenter_service_update_failed:'] = 'Lapòs aktyalizasyon echwe:';
+$lang['clearcenter_subscription_update_complete'] = 'Abònman aktyalizasyon konplè';
+$lang['clearcenter_system_not_registered'] = 'Sistèm pa anrejistre.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Kapab kontakte sèvè aleka.';
