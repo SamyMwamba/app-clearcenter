aef075f286cfca0864e88c91574296aa24ad6ce8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:22:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Osnovni sistem zagotavlja jedro nabor orodij za programsko opremo iz ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Osnovna';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Povezovanje z ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Povezava ni uspela:';
+$lang['clearcenter_dns_lookup_failed'] = 'Iskanje DNS ni uspelo.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Podatki iz daljinskega omrežja ni na voljo.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Preostali Uporabniške licence:';
+$lang['clearcenter_service_is_up_to_date'] = 'Storitev je up-to-datum.';
+$lang['clearcenter_service_requires_an_update'] = 'Storitev zahteva posodobitev.';
+$lang['clearcenter_service_update_failed:'] = 'posodobitev storitev ni uspelo:';
+$lang['clearcenter_subscription_update_complete'] = 'Posodobitev naročnina končana';
+$lang['clearcenter_system_not_registered'] = 'Sistem ni registriran.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Ni mogoče, da se obrnete oddaljenega strežnika.';
