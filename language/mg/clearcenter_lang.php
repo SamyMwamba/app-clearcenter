a5287ac254b912f5192609e99bb6d8f7ecbe06d8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:19:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Ny tena ratsy dia manome rafitra fototry teo ny fitaovana ho an&#39;ny rindrambaiko avy ClearCenter.';
+$lang['clearcenter_app_name'] = 'ClearCenter Base';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Mampifandray ny ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Connection Tsy nahomby:';
+$lang['clearcenter_dns_lookup_failed'] = 'DNS lookup nahomby.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Information avy lavitra tambajotra dia tsy misy.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Misy fahazoan-dalana sisa User:';
+$lang['clearcenter_service_is_up_to_date'] = 'Service dia niakatra-to-daty.';
+$lang['clearcenter_service_requires_an_update'] = 'Service dia mitaky ny fanavaozam-baovao.';
+$lang['clearcenter_service_update_failed:'] = 'Service vaovao farany Tsy nahomby:';
+$lang['clearcenter_subscription_update_complete'] = 'Famandrihana fanavaozam-baovao feno';
+$lang['clearcenter_system_not_registered'] = 'System tsy voasoratra anarana.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Tsy afaka hifandray lavitra mpizara.';
