a155dcd5bb85a68c93428fa1c25c9016e3012e85
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:24:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/clearcenter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['clearcenter_app_description'] = 'Системаи пойгоҳи аслӣ маҷмӯи воситаҳои барномавӣ аз ClearCenter таъмин менамояд.';
+$lang['clearcenter_app_name'] = 'ClearCenter Пойгоҳи';
+$lang['clearcenter_connecting_to_clearcenter'] = 'Пайвастшавӣ ба ClearCenter ...';
+$lang['clearcenter_connection_failed:'] = 'Хатогӣ дар пайвастшавӣ:';
+$lang['clearcenter_dns_lookup_failed'] = 'обратного DNS нашуд.';
+$lang['clearcenter_information_from_remote_network_not_available'] = 'Маълумот аз шабакаи дурдаст мавҷуд нест.';
+$lang['clearcenter_remaining_user_licenses:'] = 'Боқимонда литсензияи корбар:';
+$lang['clearcenter_service_is_up_to_date'] = 'Хадамоти аст, то-ба-санаи.';
+$lang['clearcenter_service_requires_an_update'] = 'Хадамоти талаб нав кунед.';
+$lang['clearcenter_service_update_failed:'] = 'навсозии Хадамоти ноком:';
+$lang['clearcenter_subscription_update_complete'] = 'Навсозии абонентии пурра';
+$lang['clearcenter_system_not_registered'] = 'Системаи аст, ба қайд гирифта намешавад.';
+$lang['clearcenter_unable_to_contact_remote_server'] = 'Қодир ба тамос ки хидматрасони дурдаст.';
